69556cd3cb3352dd16e8d65cedabb981f1b4f677
dat <huynhngocdat2002.td@gmail.com>
2022-04-06 21:33:15 +0700

    update-app.js

--- app.js
+++ app.js
@@ -8,25 +8,58 @@ form.addEventListener('submit', (e)=>{
     console.log(input.value);
     if(val){
         addTodos({
-            text:val,
-            status:'comleted'
-        })
-        addTodos({
             text:val
         })
     }
+    else{
+        alert('không có dữ liệu')
+    }
+    input.value = ' '
+    saveLocalstorage();
 })
 function addTodos(todo) {
     let li = document.createElement('li')
     li.innerHTML = `<span>${todo.text}</span> 
                     <i class="fa-solid fa-trash-can"></i>
                     `
-     li.setAttribute('class',todo.status)
-     console.log(li);
+
+    if(todo.status === 'completed'){
+        li.setAttribute('class',todo.status)
+    }
+    li.addEventListener('click',function(){
+        this.classList.toggle('completed')
+        saveLocalstorage()
+    })
+    li.querySelector('i').addEventListener('click',function(){
+        console.log(this.parentElement.nodeType);
+        this.parentElement.remove()
+        saveLocalstorage()
+    })
     todos.appendChild(li)
-    // todos.innerHTML = ` <li class="${todo.status}">
-    //                         <span>${todo.text}</span>
-    //                         <i class="fa-solid fa-trash-can"></i>
-    //                     </li>
-    //                     `
 }
+
+// save localstorage
+
+function saveLocalstorage() {
+    let itemTodo = document.querySelectorAll('li');
+    let todoStorage = []
+     itemTodo.forEach((item)=>{
+        let text = item.querySelector('span').innerText;
+        let status = item.querySelector('span').getAttribute('class')
+        todoStorage.push({
+            text,
+            status
+        })
+    })
+    localStorage.setItem('todoList',JSON.stringify(todoStorage))
+    // console.log(JSON.parse( localStorage.getItem('todoList')));
+ }
+
+ function init() {
+     let data = JSON.parse( localStorage.getItem('todoList'))
+    data.forEach(function(item){
+        addTodos(item)
+    })
+ }
+
+ init();
